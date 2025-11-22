54faf7e20072aa16443fb3d8f84abefa289d7e5b
kursova <gizemkursova@gmail.com>
2025-11-23 01:22:18 +0300

    feat: Update hello message version to deneme_10

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from version Gule Gule K8s2!deneme_9"
+    return "Hello from version Gule Gule K8s2!deneme_10"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
