5af90264a591c1b462eca679f531e08d1c15cda3
kursova <gizemkursova@gmail.com>
2025-11-23 10:43:42 +0300

    chore: Update displayed application version from v5 to v6.

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def create_app():
     @app.route("/")
     def hello():
         env = os.getenv("APP_ENV", "dev")
-        return f"Hello from {env} environment!_v5"
+        return f"Hello from {env} environment!_v6"
 
     @app.route("/health")
     def health():
