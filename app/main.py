ca93e6126f5ccf7ca0be7754f2f603d2f5d9046d
kursova <gizemkursova@gmail.com>
2025-11-23 08:56:57 +0300

    feat: Add health endpoint, implement environment-aware greeting, and refactor app initialization into a function.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+# main.py
+from flask import Flask, jsonify
+import os
 
-@app.route("/")
-def hello():
-    return "Hello from version Gule Gule K8s2!deneme_10"
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        env = os.getenv("APP_ENV", "dev")
+        return f"Hello from {env} environment!"
+
+    @app.route("/health")
+    def health():
+        return jsonify(status="ok"), 200
+
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
